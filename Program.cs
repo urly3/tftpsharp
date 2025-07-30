b074d96d3691f4cb7b57102f028f46aaccf59f7a
urly3 <u@rl.yyy>
2025-07-30 19:41:30 +0100

    handle errors and retry unacknowledged packets take host, port, filename from args
    ParseResult to be able to pass back data of unknown response
    - could probably be cleaner but i don't care

--- Program.cs
+++ Program.cs
@@ -2,4 +2,62 @@
 
 TftpClient client = new();
 
-client.SendFile(IPAddress.Parse("127.0.0.1"), 69, "test.txt", "octet");
+// tftpsharp 127.0.0.1 69 test.txt
+
+if (args.Length == 1)
+{
+    if (args[0] == "-h" || args[0] == "--help")
+    {
+        Console.WriteLine(
+            """
+            tftpsharp: simple tftp client in c#
+
+            note:
+            always uses octet mode
+            will retry once after a 3sec timout
+
+            tftpsharp host_ip host_port filepath
+              - host_ip: ipv4 address
+              - host_port: udp port, usually 69
+              - filepath: relative or absolute file path
+            """
+        );
+        return 1;
+    }
+
+    Console.WriteLine("tftpsharp: unknown arg");
+    return 1;
+}
+if (args.Length != 3)
+{
+    Console.WriteLine(
+        """
+        tftpsharp host_ip host_port filepath
+        """
+    );
+    return 1;
+}
+
+if (!IPAddress.TryParse(args[0], out IPAddress? host))
+{
+    Console.WriteLine("tftpsharp: invalid host_ip");
+    return 1;
+}
+
+if (!ushort.TryParse(args[1], out ushort host_port))
+{
+    Console.WriteLine("tftpsharp: invalid host_port");
+    return 1;
+}
+
+string filePath = args[2];
+
+if (!File.Exists(filePath))
+{
+    Console.WriteLine("tftpsharp: file does not exist");
+    return 1;
+}
+
+client.SendFile(host, host_port, filePath, "octet");
+
+return 0;
--- TftpClient.cs
+++ TftpClient.cs
@@ -1,98 +1,228 @@
+using System.Buffers.Binary;
+using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
-
-// info:
-// string is netascii with a null termination (the 0byte)
-//
-// rrq/wrq packets:
-// 2bytes opcode (1, 2) | filename string | 0byte | mode string | 0byte
-// modes:
-// netascii, octet, mail (deprecated)
-//
-// data packets:
-// 2bytes opcode (3) | 2bytes block# | nbytes data
-// 512 n indicates there are more blocks to come, <=511 == EOF
-// i.e. packet < 516
-//
-// ack packet:
-// 2bytes opcode (4) | 2bytes block#
-//
-// error packet:
-// 2bytes opcode (5) | 2bytes errorcode | string err | 0byte
+using System.Text;
 
 class TftpClient
 {
-    public void SendFile(IPAddress remote, int port, string filepath, string mode)
+    public void SendFile(IPAddress remote, int port, string filePath, string mode)
     {
-        IPEndPoint tftpServer = new(remote, 69);
-        // start the connection to specified host and port
-        UdpClient connection = new();
+        int maxBlockSize = 512;
+        byte[] buffer = new byte[maxBlockSize + 4];
+        IPEndPoint tftpServer = new(remote, port);
+        IPEndPoint? responder = null;
 
-        // send a wrq packet to initiate the connection
-        byte[] buffer = new byte[516];
+        string filename = Path.GetFileName(filePath);
 
-        byte[] opcode = BitConverter.GetBytes((ushort)2);
+        UdpClient conn = new();
+        conn.Client.ReceiveTimeout = 3000;
+        int retryCount = 2;
 
-        buffer[0] = opcode[1];
-        buffer[1] = opcode[0];
+        Console.CursorLeft = 0;
+        Console.Write("                        ");
+        Console.CursorLeft = 0;
+        Console.Write($"sending: wrq");
+        SendRequest(buffer, conn, tftpServer, OpCode.Wrq, filename, mode);
 
-        int cur = 2;
-
-        for (int i = 0; i < filepath.Length; i++, cur++)
+        ParseResult res = ParseResponse(conn, ref responder);
+        if (res.Op != OpCode.Ack && res.BlockNumber != 0)
         {
-            buffer[cur] = (byte)filepath[i];
+            SendError(buffer, conn, responder, ErrorCode.Undefined, "bad ack");
+            return;
         }
 
-        buffer[cur] = 0;
-        cur++;
+        using FileStream file = File.OpenRead(filePath);
+        int bytesRead = 0;
+        ushort currentBlock = 1;
+        int outputBlock = 1;
+        int resends = 0;
 
-        for (int i = 0; i < mode.Length; i++, cur++)
-        {
-            buffer[cur] = (byte)mode[i];
-        }
+        Console.CursorLeft = 0;
+        Console.CursorVisible = false;
+        Console.Write("sending: block #");
 
-        buffer[cur] = 0;
-        cur++;
-
-        connection.Send(buffer, 4 + filepath.Length + mode.Length, tftpServer);
-
-        // (wait to)receive the ack packet, and send data in a loop
-        // start from block 1, sending the data;
-        // re-send if no ack, retry a few times
-        // TODO: timeouts and re-sending
-        IPEndPoint? remoteEndpoint = null;
-
-        using FileStream file = File.OpenRead(filepath);
-        int read = 0;
-        ushort currentBlock = 0;
-
+        Stopwatch timer = new();
+        timer.Start();
         do
         {
-            byte[] ack = connection.Receive(ref remoteEndpoint);
-            Array.Reverse(ack);
-            if (BitConverter.ToUInt16(ack) != currentBlock)
-            {
-                Console.WriteLine(
-                    $"got ack for block {BitConverter.ToUInt16(ack)}, wanted {currentBlock}"
-                );
-                throw new Exception("incorrect block acknowledged");
-            }
-            currentBlock++;
-
             // set up data packet
-            opcode = BitConverter.GetBytes((ushort)3);
-            buffer[0] = opcode[1];
-            buffer[1] = opcode[0];
+            BinaryPrimitives.WriteUInt16BigEndian(buffer.AsSpan(), (ushort)OpCode.Data);
+            BinaryPrimitives.WriteUInt16BigEndian(buffer.AsSpan(2), currentBlock);
 
-            byte[] blockNumBytes = BitConverter.GetBytes((ushort)currentBlock);
-            buffer[2] = blockNumBytes[1];
-            buffer[3] = blockNumBytes[0];
+            bytesRead = file.Read(buffer, 4, maxBlockSize);
 
-            read = file.Read(buffer, 4, 512);
+            for (int i = 1; i <= retryCount; i++)
+            {
+                Console.CursorLeft = 16;
+                Console.Write(outputBlock);
+                conn.Send(buffer, 4 + bytesRead, responder);
 
-            connection.Send(buffer, 4 + read, remoteEndpoint);
-        } while (read > 0);
+                res = ParseResponse(conn, ref responder);
+
+                if (res.Op == OpCode.Ack && res.BlockNumber == currentBlock)
+                {
+                    outputBlock++;
+                    currentBlock++;
+                    break;
+                }
+
+                if (res.Op == OpCode.Error || i >= retryCount)
+                {
+                    if (res.Op != OpCode.Error)
+                    {
+                        SendError(buffer, conn, responder, ErrorCode.Undefined, "bad ack");
+                    }
+                    timer.Stop();
+
+                    outputBlock--;
+
+                    Console.CursorLeft = 0;
+                    Console.Write("                        ");
+                    Console.CursorLeft = 0;
+                    Console.WriteLine("tftpsharp: transfer failed");
+                    if (res.Op == OpCode.Error)
+                    {
+                        Console.WriteLine($"err received: {res.ErrorMessage}");
+                    }
+                    Console.WriteLine(
+                        $"  sent {outputBlock} blocks, {(512 * (outputBlock - 1) + bytesRead)} bytes, {resends} resends"
+                    );
+                    Console.WriteLine($"  elapsed time: {timer.ElapsedMilliseconds} ms");
+                    Console.CursorVisible = true;
+                    return;
+                }
+
+                resends++;
+            }
+        } while (bytesRead == maxBlockSize);
+        timer.Stop();
+
+        outputBlock--;
+
+        Console.CursorLeft = 0;
+        Console.Write("                        ");
+        Console.CursorLeft = 0;
+        Console.WriteLine("tftpsharp: transfer complete");
+        Console.WriteLine(
+            $"  sent {outputBlock} blocks, {(512 * (outputBlock - 1) + bytesRead)} bytes, {resends} resends"
+        );
+        Console.WriteLine($"  elapsed time: {timer.ElapsedMilliseconds} ms");
+        Console.CursorVisible = true;
 
         return;
     }
+
+    void SendRequest(
+        byte[] buffer,
+        UdpClient conn,
+        IPEndPoint endpoint,
+        OpCode type,
+        string fileName,
+        string mode
+    )
+    {
+        int totalLength = 4 + fileName.Length + mode.Length;
+        BinaryPrimitives.WriteUInt16BigEndian(buffer, (ushort)OpCode.Wrq);
+
+        byte[] nameBytes = Encoding.UTF8.GetBytes(fileName);
+        byte[] modeBytes = Encoding.UTF8.GetBytes(mode);
+
+        nameBytes.AsSpan().CopyTo(buffer.AsSpan(2));
+        modeBytes.AsSpan().CopyTo(buffer.AsSpan(2 + nameBytes.Length + 1));
+
+        conn.Send(buffer, totalLength, endpoint);
+    }
+
+    void SendError(
+        byte[] buffer,
+        UdpClient conn,
+        IPEndPoint? endpoint,
+        ErrorCode errCode,
+        string errorMessage
+    )
+    {
+        int totalLength = 4 + errorMessage.Length;
+        BinaryPrimitives.WriteUInt16BigEndian(buffer, (ushort)OpCode.Error);
+        BinaryPrimitives.WriteUInt16BigEndian(buffer.AsSpan(2), (ushort)errCode);
+
+        Encoding.UTF8.GetBytes(errorMessage).CopyTo(buffer, 4);
+        buffer[totalLength] = 0;
+
+        conn.Send(buffer, totalLength + 1, endpoint);
+    }
+
+    ParseResult ParseResponse(UdpClient conn, ref IPEndPoint? endpoint)
+    {
+        byte[] buffer = [];
+        try
+        {
+            buffer = conn.Receive(ref endpoint);
+        }
+        catch
+        {
+            return new ParseResult() { Op = OpCode.None };
+        }
+
+        ushort recvOp = BinaryPrimitives.ReadUInt16BigEndian(buffer);
+        OpCode op = (OpCode)recvOp;
+
+        switch (op)
+        {
+            case OpCode.Ack:
+            {
+                ushort blockNum = BinaryPrimitives.ReadUInt16BigEndian(buffer.AsSpan(2));
+                return new ParseResult() { Op = op, BlockNumber = blockNum };
+            }
+            case OpCode.Error:
+            {
+                ushort errorCode = BinaryPrimitives.ReadUInt16BigEndian(buffer.AsSpan(2));
+                string errorMessage = Encoding.UTF8.GetString(buffer.AsSpan(4));
+                return new ParseResult()
+                {
+                    Op = op,
+                    Err = (ErrorCode)errorCode,
+                    ErrorMessage = errorMessage,
+                };
+            }
+            default:
+            {
+                break;
+            }
+        }
+
+        return new ParseResult() { Op = OpCode.Error };
+    }
+}
+
+enum OpCode : ushort
+{
+    None,
+    Rrq,
+    Wrq,
+    Data,
+    Ack,
+    Error,
+}
+
+enum ErrorCode : ushort
+{
+    Undefined,
+    FileNotFound,
+    AccessViolation,
+    DiskFull,
+    IllegalOp,
+    UnknownId,
+    FileExists,
+    NoSuchUser,
+}
+
+class ParseResult
+{
+    public OpCode Op = OpCode.None;
+    public ErrorCode? Err = null;
+    public ushort? BlockNumber = null;
+    public string? ErrorMessage = null;
+
+    public ParseResult() { }
 }
